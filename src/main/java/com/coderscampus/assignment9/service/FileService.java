a4784e56aa81c325f62f8ec8882bc20fb55d2462
nwood313 <161774016+nwood313@users.noreply.github.com>
2024-09-30 01:22:53 -0400

    saving commit

--- src/main/java/com/coderscampus/assignment9/service/FileService.java
+++ src/main/java/com/coderscampus/assignment9/service/FileService.java
@@ -3,16 +3,15 @@ package com.coderscampus.assignment9.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-
-import java.io.FileReader;
-import java.io.Reader;
 
 @Service
 public class FileService {
@@ -22,16 +21,21 @@ public class FileService {
 
     public List<String[]> readRecipes() throws IOException {
         List<String[]> recipes = new ArrayList<>();
-        try (FileReader fileReader = new FileReader(filePath);
-             Reader reader = fileReader;
-             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-                     .withHeader()
-                     .withIgnoreSurroundingSpaces())) {
-            for (CSVRecord csvRecord : csvParser) {
+
+        try (Reader reader = new FileReader(filePath);
+             CSVParser parser = new CSVParser(reader,
+                     CSVFormat.DEFAULT
+                             .withDelimiter(',')
+                             .withQuote('"')
+                             .withHeader("")
+                             .withTrim()
+                             .withIgnoreHeaderCase())) {
+            for (CSVRecord csvRecord : parser) {
                 String[] recipeData = csvRecord.toMap().values().toArray(new String[0]);
                 recipes.add(recipeData);
             }
         }
+
         return recipes;
     }
 }
